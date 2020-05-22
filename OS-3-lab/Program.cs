b92fc328a952b718b286a8299e32f049e8de53b2
tumbashka <tumbashka@gmail.com>
2020-05-22 12:08:54 +0400

    Добавьте файлы проекта.

---
+++ OS-3-lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_3_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
